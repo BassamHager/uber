66a95164bdad52420a11ca6e4e175c4b26bd8ed9
bassamahager <bassam@kaskade.finance>
2024-10-16 10:14:22 +0400

    WIP: Register & Auth
    - Install & set up clerk
    - Install rn-modal
    - Update layout files
    - Create signing screens
    - Create InputField component
    - Update welcome & home screens
    - Create auth lib

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -4,8 +4,8 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="welcome" options={{ headerShown: false }} />
-      <Stack.Screen name="signUp" options={{ headerShown: false }} />
-      <Stack.Screen name="signIn" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
     </Stack>
   );
 }
